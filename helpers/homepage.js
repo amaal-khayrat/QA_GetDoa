1946341ce8a2db9dc26b841236b23cbcf44797a7
Aerry Asmani <aerryasmani@gmail.com>
2026-01-13 13:51:50 +0800

    - Added Cors exception filter for logs - Re-structure the files

--- helpers/homepage.js
+++ helpers/homepage.js
@@ -3,6 +3,14 @@ import { expect } from '@playwright/test';
 const PageTitle = "GetDoa - Your Personalized Prayer Journey";
 const PageMetaTag ="Immerse yourself in a sanctuary of digital serenity. Access authentic Doa, curate your daily supplications, and connect with the divine through a beautifully crafted experience."
 
+
+export async function GeneralHomepage (Page){
+    const LogoLocator = page.getByRole('img', { name: 'GetDoa Logo' }).first();
+    await expect(LogoLocator).toBeVisible();
+
+    await LogoLocator.click();
+}
+
 export async function VerifySEOElement(page) {
     await expect(page).toHaveTitle(PageTitle);
 
@@ -16,4 +24,56 @@ export async function VerifyLogoVisibility (page) {
 
     await expect(LogoLocator).toHaveAttribute('alt', 'GetDoa Logo');
     await expect(LogoLocator).toHaveAttribute('src', '/logo.svg');
+}
+
+export async function HeroSectionText (page) {
+    const HeroHeadingText = page.getByRole('heading', { name: 'Create Your Personalized Prayer'})
+
+    await expect(HeroHeadingText).toBeVisible();
+    await expect(page.getByText('Immerse yourself in a sanctuary of digital serenity. Access authentic Doa and curate your daily supplications.')).toBeVisible(); 
+}
+
+export async function Button_primeCTA (page) {
+    const PrimeHeroBtn= page.getByRole('link', { name: 'Create Your Doa List' }).first();
+
+    await expect(PrimeHeroBtn).toBeVisible(); 
+    await expect(PrimeHeroBtn).toHaveAttribute('href', '/onboarding');
+}
+
+export async function ClickPrimeCTA (page) {
+    const primeHeroBtn= page.getByRole('link', { name: 'Create Your Doa List' }).first();
+
+    await expect(primeHeroBtn).toBeVisible();
+    
+    await Promise.all([
+        page.waitForURL('**/onboarding**', { timeout: 10000 }),
+        primeHeroBtn.click()
+    ]);
+
+    await expect(page).toHaveURL('https://getdoa.com/login?ref=%2Fonboarding');
+    await page.goBack();
+
+    await expect(page).toHaveURL('https://getdoa.com');
+}
+
+export async function Button_SecondaryCTA(page) {
+    const SecondaryHeroBtn= page.getByRole('link', { name: 'See Doa Library' })
+
+    await expect(SecondaryHeroBtn).toBeVisible(); 
+    await expect(SecondaryHeroBtn).toHaveAttribute('href', '/doa');
+}
+
+export async function ClickSecondaryCTA(page) {
+    const SecondaryHeroBtn = page.getByRole('link', { name: 'See Doa Library' })
+
+    await expect(SecondaryHeroBtn).toBeVisible(); 
+    
+    await Promise.all([
+        page.waitForURL('https://getdoa.com/doa', { timeout: 300000 }), // 60 seconds
+        SecondaryHeroBtn.click(),
+    ]);
+    await page.waitForLoadState('domcontentloaded')
+    await expect(page.getByText('Blessing in Wealth and Offspring Doa')).toBeVisible(); 
+    await expect(page).toHaveURL('https://getdoa.com/doa');
+
 }
--- tests/Login/homepage.spec.js
+++ tests/Login/homepage.spec.js
@@ -1,7 +1,13 @@
 import { test, expect } from '@playwright/test'; 
 import {
+    GeneralHomepage,
     VerifySEOElement,
-    VerifyLogoVisibility
+    VerifyLogoVisibility,
+    HeroSectionText,
+    Button_primeCTA,
+    ClickPrimeCTA,
+    Button_SecondaryCTA,
+    ClickSecondaryCTA
 } from '../../helpers/homepage';
 
 import {
@@ -14,25 +20,58 @@ const BaseUrl = 'https://getdoa.com';
 
 //Browser Setup
 test.beforeEach(async ({ page }) => {
-  await page.goto(BaseUrl);
+    // Suppress CORS errors from console
+    page.on('console', msg => {
+        if (msg.type() === 'error' && msg.text().includes('CORS')) {
+            return; // Ignore CORS errors
+        }
+        console.log(msg.text());
+    });
+
+    await page.goto(BaseUrl);
 });
 
 //Test Cases
 test.describe('Homepage Tests', () => {
+
+    test.describe('Main Navigation', () =>{
+
+        test('CF-001 | Homepage | Verify SEO Present', async ({ page }) => {
+            await VerifySEOElement(page);
+        });
+
+        test('CF-002 | Homepage | Top Navigatio is present and visible', async ({ page }) => {
+            await VerifySEOElement(page);
+            await VerifyLogoVisibility (page);
+            await VerifySSOButton (page);
+        });
+
+        test('CF-003 | Homepage | Login Redirection', async ({ page }) => {
+            await VerifySSOButton (page);
+            await RedirectLogin (page);
+        });
+
+    });
+
+    test.describe('Hero Section', () =>{
+
+        test('CF-004 | Homepage | VerifyHero Section is present', async ({ page }) => {
+            await HeroSectionText(page);
+            await Button_primeCTA(page);
+            await Button_SecondaryCTA(page);
+            
+        });
+
+        test('CF-005 | Homepage | Prime CTA is present and navigates correctly', async ({ page }) => {
+            await ClickPrimeCTA(page);
+            await VerifyLogoVisibility (page);
+            await VerifySSOButton (page);
+        });
+
+        test('CF-006 | Homepage | Secondary CTA buttons navigates correctly', async ({ page }) => {
+            await ClickSecondaryCTA(page);
+        });
+        
+    });
     
-    test('CF-001 | Homepage | Verify SEO Present', async ({ page }) => {
-        await VerifySEOElement(page);
-    });
-
-    test('CF-002 | Homepage | Top Navigatio is present and visible', async ({ page }) => {
-        await VerifySEOElement(page);
-        await VerifyLogoVisibility (page);
-        await VerifySSOButton (page);
-    });
-
-    test('CF-003 | Homepage | Login Redirection', async ({ page }) => {
-        await VerifySSOButton (page);
-        await RedirectLogin (page);
-    });
-
 });
